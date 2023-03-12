4715f3fd9e20f4ae33af6f3e6bc47c5f03708236
lucasyan420 <lucasyan2005@gmail.com>
2023-03-12 11:17:23 +0800

    Test 1

---
+++ src/Domino.java
@@ -0,0 +1,38 @@
+public class Domino {
+    private int top;
+    private int bottom;
+
+    public Domino(){
+        top = 0;
+        bottom = 0;
+    }
+
+    public Domino(int top, int bottom){
+        this.top = top;
+        this.bottom = bottom;
+    }
+
+    public int getTop() {
+        return top;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(int bottom) {
+        this.bottom = bottom;
+    }
+
+    @Override
+    public String toString() {
+        return "Domino{" +
+                top + "/" + bottom +
+                '}';
+    }
+}
+
